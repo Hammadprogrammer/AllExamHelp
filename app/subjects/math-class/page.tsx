5d151139146b1847ce6c12a2ba5f9a93f992c9ef
hammadprogrammer <hammadzahid221@gmail.com>
2026-04-04 21:36:38 +0500

    math page

--- app/subjects/math-class/page.tsx
+++ app/subjects/math-class/page.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import type { Metadata } from 'next'
+import OnlineClassHero from '@/container/all-subject/math/hero_section/page'
+import UniSection from '@/container/home/uni_section/uni_section'
+import Features from '@/container/all-subject/math/feature_section/feature_section'
+import ContentSectionOne from '@/container/all-subject/math/Content_section_one/Content_section_one'
+import CallActionSection from '@/container/all-subject/math/call_action/call_acition'
+import Stats_section from '@/container/all-subject/math/stats_section/stats_section'
+import Content_section_two from '@/container/all-subject/math/content_section_two/content_section_two'
+import ProcessSection from '@/container/home/process_section/process_section'
 import ServicesSection from '@/container/home/service_section/service_section'
-import WorkProcess from '@/container/home/process_section/process_section'
+import ContentSectionThree from '@/container/all-subject/math/content_section_three/content_section_three'
+
+// Meta Data Configuration
+export const metadata: Metadata = {
+  title: "Online Math Class, Pay Someone to Do My Math Class for Me",
+  description: "Struggling to keep up in math? Get expert online math class help and improve your grades quickly with professional guidance. Get 50% Discount Offer on Your first math class order today.",
+}
 
 const page = () => {
   return (
     <>
-      
-      <WorkProcess />
-      <ServicesSection />
+      <OnlineClassHero />
+      <UniSection/>
+      <Features />
+      <ContentSectionOne />
+      <CallActionSection/>
+      <Stats_section/>
+      <Content_section_two/>
+      <ProcessSection/>
+      <ServicesSection/>
+      <ContentSectionThree/>
     </>
   )
 }
---
+++ container/all-subject/math/content_section_two/content_section_two.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContentTwo from '@/container/All-props-comp/content_section_two/content_section_two'
+
+const Content_section_two = () => {
+  return (
+    <>
+      <ContentTwo 
+        heading="Take Online Math Class Help and Never Miss a Deadline Again- 100% Confidential and Secure "
+        description="Keeping up with assignments in any academic setting today can be tough. When you opt for a math class taker, you know that all of your classwork, discussion and projects will be done right on time. We can offer help with Math class solutions to provide you with a professional way for calculus, algebra, statistics and many more, according to your class. All the work is done with a policy of confidentiality, thus your personal information and rates are secure. Thanks to expert tutors organizing your classwork, you have more time to learn at your own speed and will manage less pressure, leading to better grades without the fear of deadlines."
+      />
+    </>
+  )
+}
+
+export default Content_section_two;
